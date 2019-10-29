862860e66761e3e651b43e489b34c29673048ad0
morningreis <morningreis@outlook.com>
2019-10-28 20:07:14 -0400

    Disables Linear Advance

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -938,7 +938,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { -50, -16, -1.025 }
+#define NOZZLE_TO_PROBE_OFFSET { -50, -16, -1.075 }
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 10
--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1346,10 +1346,10 @@
  * See http://marlinfw.org/docs/features/lin_advance.html for full instructions.
  * Mention @Sebastianv650 on GitHub to alert the author of any issues.
  */
-#define LIN_ADVANCE
+//#define LIN_ADVANCE
 #if ENABLED(LIN_ADVANCE)
   //#define EXTRA_LIN_ADVANCE_K // Enable for second linear advance constants
-  #define LIN_ADVANCE_K 0.22    // Unit: mm compression per 1mm/s extruder speed
+  #define LIN_ADVANCE_K 0.66    // Unit: mm compression per 1mm/s extruder speed
   //#define LA_DEBUG            // If enabled, this will generate debug information output over USB.
 #endif
 
@@ -2068,7 +2068,7 @@
 
   #if EITHER(SENSORLESS_HOMING, SENSORLESS_PROBING)
     // TMC2209: 0...255. TMC2130: -64...63
-    #define X_STALL_SENSITIVITY  4
+    #define X_STALL_SENSITIVITY  5
     #define X2_STALL_SENSITIVITY X_STALL_SENSITIVITY
     #define Y_STALL_SENSITIVITY  5
     //#define Z_STALL_SENSITIVITY  8
